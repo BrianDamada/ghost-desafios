a8db0442338e55760796a4170f47bdb131e350bf
BrianDamada <114277605+BrianDamada@users.noreply.github.com>
2025-07-11 17:02:30 -0300

    atualização do paine

--- src/app/utils/routes/publicsRoutes.js
+++ src/app/utils/routes/publicsRoutes.js
@@ -4,6 +4,10 @@ let PublicRotas = {
       title: "Baixe-me se for capaz!",
       path: "/baixe-me_se_for_capaz",
     },
+    {
+      title: "Me deixe em 100",
+      path: "/chegue_a_100",
+    },
   ],
 };
 
